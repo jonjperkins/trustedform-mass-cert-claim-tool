23498859dab061ec7535a42b32dd91c16410fa5b
Jon Perkins <jjperk@gmail.com>
2017-09-28 12:25:06 -0500

    style changes

--- src/Results.js
+++ src/Results.js
@@ -6,25 +6,25 @@ import{Row, Col} from 'react-bootstrap';
 
 class Results extends Component {
 	render() {
-    	return (
-    		<Row>
-    			<Col md={6} style={{textAlign: "center"}}>
-    				<ResultCount
-                    className="successful" 
-                    number={this.props.numberSuccessfulResubmits} 
-                    text="Successful Claims" 
-                    />
-    			</Col>
-    			<Col md={6} style={{textAlign: "center"}}>
-    				<ResultCount 
-                    className="unsuccessful" 
-                    number={this.props.numberUnsuccessfulResubmits} 
-                    text="Unsuccessful Claims"
-                    />
-    			</Col>
-    		</Row>
-		);
-    }
+    return (
+      <Row>
+        <Col md={6} style={{textAlign: "center"}}>
+          <ResultCount
+            className="successful" 
+            number={this.props.numberSuccessfulResubmits} 
+            text="Successful Claims" 
+          />
+        </Col>
+        <Col md={6} style={{textAlign: "center"}}>
+          <ResultCount 
+            className="unsuccessful" 
+            number={this.props.numberUnsuccessfulResubmits} 
+            text="Unsuccessful Claims"
+          />
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default Results;
