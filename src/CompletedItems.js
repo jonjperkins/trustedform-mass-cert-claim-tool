0c83e080aad72fceaf8ab95dd5c57e6b686fd938
Jon Perkins <jjperk@gmail.com>
2017-09-28 12:16:07 -0500

    style changes

--- src/CompletedItems.js
+++ src/CompletedItems.js
@@ -4,32 +4,31 @@ import './App.css';
 
 class CompletedItems extends Component {
 	render() {
-    	return (
-      		<div className="list-div"> 
-      		    {	this.props.accountsResubmittedArray.length >= 1 &&
-                  this.props.accountsResubmittedArray.map((account) => 				
-    						      <ChecklistItem 
-                          account={account}
-                          name="check-circle" 
-                          size="2x" 
-                          className={this.props.className} 
-                          textColor="dark-text" 
-                      />
-    					    )
-  	    		  }
-              { this.props.accountsResubmittedArray.length < 1 && 
-                  <ChecklistItem 
-                      account="No certificates resubmitted yet" 
-                      name="check-circle" 
-                      size="2x" 
-                      className="grayed-out" 
-                      textColor="grayed-out" 
-                  />
-              }
-  	    		
-      		</div>
-    	);
-    }
+    return (
+      <div className="list-div"> 
+        {this.props.accountsResubmittedArray.length >= 1 &&
+          this.props.accountsResubmittedArray.map((account) => 				
+            <ChecklistItem 
+              account={account}
+              name="check-circle" 
+              size="2x" 
+              className={this.props.className} 
+              textColor="dark-text" 
+            />
+          )
+        }
+        { this.props.accountsResubmittedArray.length < 1 && 
+          <ChecklistItem 
+            account="No certificates resubmitted yet" 
+            name="check-circle" 
+            size="2x" 
+            className="grayed-out" 
+            textColor="grayed-out" 
+          />
+        }
+  	  </div>
+    );
+  }
 }
 
 export default CompletedItems;
